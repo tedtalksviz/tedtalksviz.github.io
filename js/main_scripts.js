6861561518b9f21b0c07f4449ea3a4d42b92970e
ErikHusgafvelEPFL <61322672+ErikHusgafvelEPFL@users.noreply.github.com>
2020-04-07 11:26:13 +0200

    Create main_scripts.js
    Created basic sceleton for main_scripts

---
+++ js/main_scripts.js
@@ -0,0 +1,18 @@
+// imports ?
+
+// constants ?
+
+// core logic
+
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
+
+whenDocumentLoaded(() => {
+  window.alert('Document fully loaded.')
+});
