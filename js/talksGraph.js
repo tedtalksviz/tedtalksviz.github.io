a129c01d0c5073db67a1b5a9dcbc15b3e406b3dd
mapaaa <maria.pandele33@gmail.com>
2020-05-22 13:23:11 +0200

    network graph + tooltip for nodes

--- js/talksGraph.js
+++ js/talksGraph.js
@@ -1,62 +1,51 @@
-// Todo make this a class
-Promise.all([
-  d3.tsv('resources/edges.tsv'),
-  d3.tsv('resources/nodes.tsv')
-]).then(createTalksGraph);
+class TalksGraph {
+  constructor(svg_element_id) {
+    this.svg = d3.select('#' + svg_element_id)
+    const network_promise = d3.json('resources/network.json');
 
-function createTalksGraph(files) {
-  links = files[0];
-  nodes = files[1];
+    Promise.all([network_promise]).then((results) => {
+      let graph = results[0];
 
-  const width = 1600;
-  const height = 800;
-  const svg = d3.select("#talks-graph");
-  svg.attr('width', width);
-  svg.attr('height', height);
+      const nodes = graph.nodes.map(function(d) {
+        return {
+          'id': parseInt(d.id),
+          'x': d.x,
+          'y': d.y,
+          'r': d.size,
+          'duration': d.attributes.duration,
+          'speaker': d.attributes.main_speaker,
+          'description': d.attributes.description,
+          'title': d.attributes.title,
+          'event': d.attributes.event
+        }
+      });
 
-  const simulation = d3.forceSimulation()
-      .nodes(nodes)
-      .force("charge", function(d) {
-        return +d.degree * 5;
-      })
-      .force("center", d3.forceCenter(width / 2, height / 2));
-  
-  // Draw circles for each node
-  const nodeElements = svg.append('g')
-    .selectAll('circle')
-    .data(nodes)
-    .enter().append('circle')
-      .attr('r', 5)
-      .attr('fill', '#FF0000');
-  
-  /* Adding text to nodes would complicate the layout
-   * TODO: figure this out
-   * const textElements = svg.append('g')
-    .selectAll('text')
-    .data(nodes)
-    .enter().append('text')
-      .text(node => node.title) */
-
-  // Draw a line for each edge
-  const linkElements = svg.append('g')
-    .selectAll('line')
-    .data(links)
-    .enter().append('line')
-      .attr('stroke-width', 1)
-      .attr('stroke', '#E5E5E5');
-
-  // Add links to our graph
-  simulation.force('link', d3.forceLink(links)
-    .id(function(d) { return d.id; })
-    .distance(120));
-  simulation.on('tick', () => {
-    linkElements
-      .attr('x1', link => link.source.x)
-      .attr('x2', link => link.target.x)
-      .attr('y1', link => link.source.y)
-      .attr('y2', link => link.target.y)
-    nodeElements
-      .attr('cx', node => node.x)
-      .attr('cy', node => node.y);
-  });
+      const tooltip = floatingTooltip('talk_tooltip', 100);
+      this.svg.selectAll('circle')
+        .on('mouseover', function(d) {
+          let node_id = this.getAttribute('class').substring(3);
+          let details = nodes.find(node => node.id == node_id);
+          var content = 
+            '<span class="name">Title: </span>' + 
+              '<span class="value">' + details.title + '</span><br/>' +
+            '<span class="name">Speaker: </span>' + 
+              '<span class="value">' + details.speaker + '</span><br/>' +
+           '<span class="name">Description: </span>' + 
+              '<span class="value">' + details.description + '</span><br/>' +
+           '<span class="name">Duration: </span>' + 
+              '<span class="value">' + Math.floor(details.duration).toString() + 
+              ' minutes </span><br/>' +
+           '<span class="name">Event: </span>' + 
+              '<span class="value">' + details.event + '</span><br/>';
+          tooltip.showTooltip(content, d3.event);
+        }) 
+        .on('mouseout', function(d) {
+          tooltip.hideTooltip();
+        });
+    });
+  }
 }
+
+whenDocumentLoaded(() => {
+  const talksGraph = new TalksGraph('talk_network');
+});
