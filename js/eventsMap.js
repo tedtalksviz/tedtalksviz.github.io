2b8b025e7f86d0b3c4ab5f482d868e561f274ae4
mapaaa <maria.pandele33@gmail.com>
2020-05-23 22:34:07 +0200

    Map work

--- js/eventsMap.js
+++ js/eventsMap.js
@@ -15,8 +15,6 @@ class EventsMap {
     // Think about a better way to set this scale.
     const projection = d3.geoMercator()
       .rotate([0, 0])
-      .scale(200)
-      .translate([this.svg_width / 2, this.svg_height / 2])
       .precision(.1);
 
     const path = d3.geoPath()
@@ -35,18 +33,52 @@ class EventsMap {
 
     const city_promise = d3.csv('resources/cities.csv');
 
-    // Will add here promises for events locations as well.
+    // Add zoom to map (this handles dragging as well)
+    const zooming = function(d) {
+      var transform = [d3.event.transform.x, d3.event.transform.y];
+      var newScale = d3.event.transform.k * 2000;
+      projection
+        .translate(transform)
+        .scale(newScale);
+      svg.selectAll('path').attr('d', path);
+      svg.selectAll('circle')
+        .attr('cx', function(d) {
+          return projection([d.longitude, d.latitude])[0];
+        })
+        .attr('cy', function(d) {
+          return projection([d.longitude, d.latitude])[1];
+        });
+    }
+    const zoom = d3.zoom()
+      .on('zoom', zooming);
+
+    
     Promise.all([map_promise, city_promise]).then((results) => {
       let map_data = results[0];
       let cities_data = results[1];
       // Countries
-      this.map_container = svg.append('g');
+      var center = projection([31, -7]);
+      this.map_container = svg
+        .append('g')
+        .attr('id', 'map')
+        .call(zoom)
+        .call(zoom.transform, d3.zoomIdentity
+            .translate(width/2, height/2)
+            .scale(0.105)
+            .translate(-center[0], -center[1]));
+      this.map_container.append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', width)
+        .attr('height', height)
+        .attr('opacity', 0);
       this.map_container.selectAll('.country')
         .data(map_data)
         .enter()
         .append('path')
         .attr('d', path)
-        .style('fill', '#793fae')
+        //.style('fill', '#793fae')
+        .style('fill', '#2a1740');
       // Cities
       this.map_container = svg.append('g');
       this.map_container.selectAll('circle')
@@ -60,7 +92,7 @@ class EventsMap {
           return projection([d.longitude, d.latitude])[1];
         })
         .attr('r', function(d) {
-          return d.no_events * 2;
+          return d.count * 2;
         })
         .style('fill', 'red')
         .style('stroke', 'gray')
@@ -69,9 +101,9 @@ class EventsMap {
         .on('mouseover', function(d) {
           var content = 
             '<span class="name">Name: </span>' + 
-              '<span class="value">' + d.name + '</span><br/>' +
+              '<span class="value">' + d.desc + '</span><br/>' +
             '<span class="name">Number of events: </span>' + 
-              '<span class="value">' + d.no_events.toString() + '</span><br/>';
+              '<span class="value">' + d.count.toString() + '</span><br/>';
           tooltip.showTooltip(content, d3.event);
         })
         .on('mouseout', function(d) {
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,6 +6,7 @@ import pandas as pd
 from time import sleep
 from random import randint
 
+
 def request_webpage(url):
     limit = 5
     i = 0
@@ -29,7 +30,8 @@ def get_location(soup, event_name):
     for row in rows[:5]:
         tds = row.findAll('td')
         if len(tds) == 5:
-            name = tds[1].find('strong').find('a').string.replace('&shy', '').replace(';\xad','').replace('\xad','')
+            name = tds[1].find('strong').find('a').string.replace(
+                '&shy', '').replace(';\xad', '').replace('\xad', '')
             location = tds[2].findAll(text=True, recursive=False)
             city = location[1].replace('\n', '')
             country = location[2]
@@ -43,13 +45,13 @@ def get_location(soup, event_name):
 def scrape(event_name, year):
     city = ''
     country = ''
-    url = 'https://www.ted.com/tedx/events?when=past&autocomplete_filter={0}&year={1}'.format(event_name, year)
+    url = 'https://www.ted.com/tedx/events?when=past&autocomplete_filter={0}&year={1}'.format(
+        event_name, year)
     page = request_webpage(url)
     if page != '':
         page_soup = BeautifulSoup(page.text, 'html.parser')
         city, country = get_location(page_soup, event_name)
     return city, country
-
 
 
 events = pd.read_csv('../resources/events.csv')
@@ -70,7 +72,5 @@ for i, row in events.iterrows():
         if city != '':
             events.loc[i, 'city'] = city
             events.loc[i, 'country'] = country
-
-
 
 events.to_csv('../data/locations_scraped.csv', index=False, header=True)
